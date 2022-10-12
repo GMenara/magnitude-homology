516a61020bec70830aa68f9709efe7ba70fd8db4
Giulia Menara <79992007+GMenara@users.noreply.github.com>
2022-10-12 13:01:40 +0200

    Add files via upload

---
+++ src-julia/main.jl
@@ -0,0 +1,67 @@
+include("/home/gmenara/JuliaProjects/boundary_map.jl")
+
+#graph=nx.icosahedral_graph()
+#graph=nx.cycle_graph(8)
+#graph.add_edge(2,6)
+#graph = nx.erdos_renyi_graph(20,0.3,seed=false,directed=false)
+
+graph = nx.cycle_graph(3)
+
+graph.add_node(3)
+graph.add_node(4)
+
+graph.add_edge(0, 3)
+graph.add_edge(0, 4)
+
+k = 2
+l = 2
+
+d_kl = boundary(graph, k, l)
+d_kplus1_l = boundary(graph, k+1, l)
+
+#define dv
+size_d_kl = size(d_kl)
+dv = Array{Int64}(undef,size_d_kl[2]) 
+for i in 1:size_d_kl[1]
+    dv[i]=k
+end
+for i in size_d_kl[1]+1:size_d_kl[2]
+    dv[i]=k+1
+end
+
+
+#check dv
+outfile = "dv.txt"
+    f = open(outfile, "w")
+
+    for i in 1:length(dv)
+        println(f,dv[i])
+        #println(f,typeof(dv[i]))
+    end
+    
+
+#define fv
+fv = ones(Int8,(1,size_d_kl[2]))
+
+dim_kernel = size_d_kl[1]- alg.matrix_rank(d_kl)
+dim_image = alg.matrix_rank(d_kplus1_l)
+betti = dim_kernel - dim_image
+#println("The dimension of the kernel of d_{k,l} for k,l=",k,l,"is",dim_kernel)
+#println("The dimension of the image of d_{k+1,l} for k,l=",k,l,"is",dim_image)
+println("betti_{k,l} for k,l=",k,l,"is", betti)
+
+S = sparse(d_kl)
+println(size(S),size(d_kl))
+println(d_kl,S)
+rv = S.rowval
+#println(rv)
+cp = S.colptr
+println(length(rv),length(cp),length(dv),length(fv))
+
+C = eirene(rv=rv, cp=cp, dv=dv, fv=fv, maxdim=k)
+
+#println(C)
+println(length(barcode(C,dim=k))) #this gives both birth time and death time 
+
+
+
